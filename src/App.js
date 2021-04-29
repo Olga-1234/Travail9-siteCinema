0e48e72b8b2661024df005ff2495ebb972aeed49
Olga Mujinga <monaolgita@gmail.com>
2021-04-29 10:34:35 +0200

    premier commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+const App = ()=> {
+  return (
+    <div className="App">
+        <p>
+        HELLO je suis olga
+        </p>
+      
+          Learn Reac
+    
+    </div>
+  );
+}
+
+export default App;
