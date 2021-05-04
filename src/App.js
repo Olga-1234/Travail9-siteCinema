44a3924f59226d682c197976b13d213d2b496e16
Olga Mujinga <monaolgita@gmail.com>
2021-05-04 18:45:32 +0200

    refactor style home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React,{useState, useEffect} from 'react';
+import Movie from "./components/Movie";
+import Navbar from "./components/Navbar"
+
+const API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6f82743b4851e8b71cb17f8d769a7941&page=2";
 
 const App = ()=> {
+  const [movies, setMovies]=useState([]);
+  useEffect (()=>{fetch(API)
+  .then(response=>response.json())
+  .then(data=>{
+    setMovies(data.results);
+    console.log(data);
+  })},[])
   return (
-    <div className="App">
-        <p>
-        HELLO je suis olga
-        </p>
+
+    <div>
+      <Navbar/>
+    {movies.length>0 && movies.map(movie=>
+      <Movie key={movie.id} {...movie}/>)}
       
-          Learn Reac
-    
+      
     </div>
+
+
   );
 }
 
